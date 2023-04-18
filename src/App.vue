6575a899444adac1b438254d7613d60996b991c5
Thomas Marrinan <thomas.j.marrinan@gmail.com>
2023-04-17 22:03:24 -0500

    almost working

--- src/App.vue
+++ src/App.vue
@@ -1,105 +1,54 @@
 <script>
 import { Engine } from '@babylonjs/core/Engines/engine';
-import { Scene } from '@babylonjs/core/scene';
-import { UniversalCamera } from '@babylonjs/core/Cameras/universalCamera';
-import { Vector3 } from '@babylonjs/core/Maths/math.vector';
-import { Color3, Color4 } from '@babylonjs/core/Maths/math.color';
 import { Mesh } from '@babylonjs/core/Meshes/mesh';
 import { VertexData } from '@babylonjs/core/Meshes/mesh.vertexData';
-import { PointLight } from '@babylonjs/core/Lights/pointLight';
-import { StandardMaterial } from '@babylonjs/core/Materials/standardMaterial';
 import { ShaderMaterial } from '@babylonjs/core/Materials/shaderMaterial';
-import { Texture } from '@babylonjs/core/Materials/Textures/texture';
-//import { CreatePlane } from '@babylonjs/core/Meshes/Builders/planeBuilder';
 
 import { Renderer } from './renderer'
 
 export default {
-    props: ['scene_desc'],
     data() {
         return {
-            gl: null,
-            scene_data: null,
-            scene: null,
-            camera: null,
-            ground_material: {
-                color: null,
-                texture: null
-            }
-
+            renderer: null
         }
     },
     methods: {
-        arrayToVector3(arr) {
-            return new Vector3(arr[0], arr[1], arr[2]);
-        },
-
-        arrayToColor3(arr) {
-            return new Color3(arr[0], arr[1], arr[2]);
-        },
-
-        arrayToColor4(arr) {
-            return new Color4(arr[0], arr[1], arr[2], arr[3]);
-        },
-
-        color3ToVector3(color) {
-            return new Vector3(color.r, color.g, color.b);
-        },
-
-        processScene(scene_desc) {
-            this.scene_data = {
-                background: this.arrayToColor4(scene_desc.background),
-                camera: {
-                    position: this.arrayToVector3(scene_desc.camera.position),
-                    target:  this.arrayToVector3(scene_desc.camera.target),
-                    up:  this.arrayToVector3(scene_desc.camera.up),
-                    near_clip: scene_desc.camera.near_clip,
-                    far_clip: scene_desc.camera.far_clip
-                },
-                ground: {
-                    shader: scene_desc.ground.shader,
-                    material: {
-                        color: this.arrayToColor3(scene_desc.ground.material.color),
-                        specular: this.arrayToColor3(scene_desc.ground.material.specular),
-                        shininess: scene_desc.ground.material.shininess
-                    },
-                    center: this.arrayToVector3(scene_desc.ground.center),
-                    size: this.arrayToVector3(scene_desc.ground.size),
-                    subdivisions: [scene_desc.ground.subdivisions[0], scene_desc.ground.subdivisions[1]],
-                    heightmap: scene_desc.ground.heightmap
-                },
-                models: [],
-                light: {
-                    ambient: this.arrayToColor3(scene_desc.light.ambient),
-                    point_lights: []
-                }
-            };
-            scene_desc.models.forEach((model_desc) => {
-                let model = {
-                    type: model_desc.type,
-                    shader: model_desc.shader,
-                    material: {
-                        color: this.arrayToColor3(model_desc.material.color),
-                        specular: this.arrayToColor3(model_desc.material.specular),
-                        shininess: model_desc.material.shininess
-                    },
-                    center: this.arrayToVector3(model_desc.center),
-                    size: this.arrayToVector3(model_desc.size),
-                    rotation: this.arrayToVector3(model_desc.rotation)
-                };
-                this.scene_data.models.push(model);
+        createBasicMaterial(name, shader_path, scene) {
+            let basic_mat = new ShaderMaterial(name, scene, shader_path, {
+                attributes: ['position', 'uv'],
+                uniforms: ['world', 'view', 'projection', 'mat_color', 'mat_specular', 'mat_shininess',
+                           'camera_position', 'ambient', 'num_lights', 'light_positions', 'light_colors'],
+                samplers: ['mat_texture']
             });
-            scene_desc.light.point_lights.forEach((light_desc) => {
-                let light = {
-                    position: this.arrayToVector3(light_desc.position),
-                    color: this.arrayToColor3(light_desc.color)
-                }
-                this.scene_data.light.point_lights.push(light);
+            basic_mat.backFaceCulling = true;
+            basic_mat.onBindObservable.add((mesh) => {
+                const shader = basic_mat.getEffect();
+                shader.setColor3('mat_color', mesh.metadata.mat_color);
+                shader.setTexture('mat_texture', mesh.metadata.mat_texture);
+                shader.setColor3('mat_specular', mesh.metadata.mat_specular);
+                shader.setFloat('mat_shininess', mesh.metadata.mat_shininess);
             });
+            return basic_mat;
         },
 
-        loadNewScene(event) {
-
+        createGroundMaterial(name, shader_path, scene) {
+            let ground_mat = new ShaderMaterial(name, scene, shader_path, {
+                attributes: ['position', 'uv'],
+                uniforms: ['world', 'view', 'projection', 'mat_color', 'mat_specular', 'mat_shininess', 'height_scalar',
+                           'camera_position', 'ambient', 'num_lights', 'light_positions', 'light_colors'],
+                samplers: ['mat_texture', 'heightmap']
+            });
+            ground_mat.backFaceCulling = false;
+            ground_mat.onBindObservable.add((mesh) => {
+                const shader = ground_mat.getEffect();
+                shader.setColor3('mat_color', mesh.metadata.mat_color);
+                shader.setTexture('mat_texture', mesh.metadata.mat_texture);
+                shader.setColor3('mat_specular', mesh.metadata.mat_specular);
+                shader.setFloat('mat_shininess', mesh.metadata.mat_shininess);
+                shader.setFloat('height_scalar', mesh.metadata.height_scalar);
+                shader.setTexture('heightmap', mesh.metadata.heightmap);
+            });
+            return ground_mat;
         },
 
         createGroundModel(name, subdivisions, scene) {
@@ -132,7 +81,7 @@ export default {
         },
 
         selectShadingAlgorithm(event) {
-
+            console.log(event);
         }
     },
     mounted() {
@@ -142,7 +91,7 @@ export default {
         canvas.height = 558;
 
         // Create a WebGL 2 rendering context
-        let gl = canvas.getContext('webgl2');
+        const gl = canvas.getContext('webgl2');
         if (!gl) {
             alert('Error: Browser does not support WebGL2 Canvas');
             return;
@@ -152,106 +101,25 @@ export default {
         const engine = new Engine(gl);
 
         // Create our Renderer
-        const renderer = new Renderer(canvas, engine, (scene) => {
-            let ground_gauroud = new ShaderMaterial('ground_gauroud', scene, '/shaders/ground_gauroud', {
-                attributes: ['position', 'uv'],
-                uniforms: ['world', 'view', 'projection'],
-                samplers: ['heightmap', 'image']
-            });
-            ground_gauroud.backFaceCulling = false;
-            let ground_phong = new ShaderMaterial('ground_phong', scene, '/shaders/ground_phong', {
-                attributes: ['position', 'uv'],
-                uniforms: ['world', 'view', 'projection'],
-                samplers: ['heightmap', 'image']
-            });
-            ground_phong.backFaceCulling = false;
-            let illum_gauroud = new ShaderMaterial('illum_gauroud', scene, '/shaders/illum_gauroud', {
-                attributes: ['position', 'uv'],
-                uniforms: ['worldViewProjection'],
-                samplers: ['image']
-            });
-            illum_gauroud.backFaceCulling = true;
-            let illum_phong = new ShaderMaterial('illum_phong', scene, '/shaders/illum_phong', {
-                attributes: ['position', 'uv'],
-                uniforms: ['worldViewProjection'],
-                samplers: ['image']
-            });
-            illum_phong.backFaceCulling = true;
-
+        this.renderer = new Renderer(canvas, engine, (scene) => {
+            let illum_gouraud = this.createBasicMaterial('illum_gouraud', '/shaders/illum_gouraud', scene);
+            let illum_phong = this.createBasicMaterial('illum_phong', '/shaders/illum_phong', scene);
+            let ground_gouraud = this.createGroundMaterial('ground_gouraud', '/shaders/ground_gouraud', scene);
+            let ground_phong = this.createGroundMaterial('ground_phong', '/shaders/ground_phong', scene);
             return {
-                ground_gauroud: ground_gauroud,
-                ground_phong: ground_phong,
-                illum_gauroud: illum_gauroud,
-                illum_phong: illum_phong
+                illum_gouraud: illum_gouraud,
+                illum_phong: illum_phong,
+                ground_gouraud: ground_gouraud,
+                ground_phong: ground_phong
             };
         }, (scene, subdivisions) => {
             return this.createGroundModel('ground', subdivisions, scene);
         });
 
-        /*
-        // Process scene description
-        this.processScene(this.scene_desc);
-        console.log(this.scene_data);
-
-        // Create our first scene.
-        this.scene = new Scene(engine);
-        this.scene.clearColor = this.scene_data.background;
-        this.scene.ambientColor = this.scene_data.light.ambient;
-
-        // Add a camera to the scene - UniversalCamera good for first-person
-        this.camera = new UniversalCamera('camera', this.scene_data.camera.position, this.scene);
-        this.camera.setTarget(this.scene_data.camera.target);
-        this.camera.upVector = this.scene_data.camera.up;
-        this.camera.attachControl(canvas, true);
-        this.camera.minZ = this.scene_data.camera.near_clip;
-        this.camera.maxZ = this.scene_data.camera.far_clip;
-
-
-        // Create custom materials for colored and textured ground models
-        this.ground_material.color = new ShaderMaterial('ground_colored', this.scene, '/shaders/ground_colored', {
-            attributes: ['position', 'uv'],
-            uniforms: ['world', 'view', 'projection'],
-            samplers: ['heightmap']
-        });
-        this.ground_material.color.backFaceCulling = false;
-        this.ground_material.texture = new ShaderMaterial('ground_textured', this.scene, '/shaders/ground_textured', {
-            attributes: ['position', 'uv'],
-            uniforms: ['world', 'view', 'projection'],
-            samplers: ['heightmap', 'image']
-        });
-        this.ground_material.texture.backFaceCulling = false;
-
-        let ground_heightmap = new Texture(this.scene_data.ground.heightmap, this.scene);
-
-        let ground = this.createGroundModel('ground', this.scene_data.ground.subdivisions, this.scene);
-        ground.scaling = this.scene_data.ground.size;
-        ground.material =  this.ground_material[this.scene_data.ground.shader];
-        ground.material.setVector3('mat_color', this.color3ToVector3(this.scene_data.ground.material.color));
-        ground.material.setVector3('mat_specular', this.color3ToVector3(this.scene_data.ground.material.specular));
-        ground.material.setFloat('mat_shininess', this.scene_data.ground.material.shininess);
-        ground.material.setFloat('height_scalar', 1.0); 
-        ground.material.setTexture('heightmap', ground_heightmap);
-        
-        /*
-        // Add light to the scene
-        let light = new PointLight('light0', new Vector3(1, 1, -5), scene);
-        light.diffuse = new Color3(1, 1, 1);
-        light.specular = new Color3(1, 1, 1);
-        
-        // Create a basic material
-        let mat = new StandardMaterial('mat');
-        mat.ambientColor = new Color4(0.2, 0.2, 0.2, 1.0);
-        mat.diffuseTexture = new Texture('/images/webglbook.png');
-        mat.specularColor = new Color4(0.1, 0.1, 0.1, 1.0);
-        
-        // Create a plane model
-        let plane = CreatePlane('plane', {width: 3.8, height: 5.0});
-        plane.material = mat;
-        */
 
         // Render every frame
         engine.runRenderLoop(() => {
-            renderer.getActiveScene().render();
+            this.renderer.getActiveScene().render();
         });
     }
 }
--- src/renderer.js
+++ src/renderer.js
@@ -1,7 +1,10 @@
 import { Scene } from '@babylonjs/core/scene';
 import { UniversalCamera } from '@babylonjs/core/Cameras/universalCamera';
+import { PointLight } from '@babylonjs/core/Lights/pointLight';
+import { CreateSphere } from '@babylonjs/core/Meshes/Builders/sphereBuilder';
 import { Texture } from '@babylonjs/core/Materials/Textures/texture';
 import { RawTexture } from '@babylonjs/core/Materials/Textures/rawTexture';
+import { Color3, Color4 } from '@babylonjs/core/Maths/math.color';
 import { Vector3 } from '@babylonjs/core/Maths/math.vector';
 
 class Renderer {
@@ -13,7 +16,10 @@ class Renderer {
                 scene: new Scene(this.engine),
                 materials: null,
                 ground_subdivisions: [50, 50],
-                ground_mesh: null
+                ground_mesh: null,
+                camera: null,
+                ambient: new Vector3(0.0, 0.0, 0.0),
+                lights: []
             }
         ];
         this.active_scene = 0;
@@ -21,29 +27,83 @@ class Renderer {
         this.scenes.forEach((scene, idx) => {
             scene.materials = material_callback(scene.scene);
             scene.ground_mesh = ground_mesh_callback(scene.scene, scene.ground_subdivisions);
-            this['createScene'+ idx](scene.scene, scene.materials, scene.ground_mesh);
+            this['createScene'+ idx](idx);
         })
     }
 
-    createScene0(scene, materials, ground_mesh) {
-        let camera = new UniversalCamera('camera', new Vector3(0.0, 1.8, 3.0), scene);
-        camera.setTarget(new Vector3(0.0, 1.8, 0.0));
-        camera.upVector = new Vector3(0.0, 1.0, 0.0);
-        camera.attachControl(this.canvas, true);
-        camera.minZ = 0.1;
-        camera.maxZ = 100.0;
+    createScene0(scene_idx) {
+        let current_scene = this.scenes[scene_idx];
+        let scene = current_scene.scene;
+        let materials = current_scene.materials;
+        let ground_mesh = current_scene.ground_mesh;
 
+        // Set scene-wide / environment values
+        scene.clearColor = new Color4(0.1, 0.1, 0.1, 1.0);
+        scene.ambientColor = new Color3(0.2, 0.2, 0.2);
+        scene.useRightHandedSystem = true;
+
+        // Create camera
+        current_scene.camera = new UniversalCamera('camera', new Vector3(0.0, 1.8, 10.0), scene);
+        current_scene.camera.setTarget(new Vector3(0.0, 1.8, 0.0));
+        current_scene.camera.upVector = new Vector3(0.0, 1.0, 0.0);
+        current_scene.camera.attachControl(this.canvas, true);
+        current_scene.camera.minZ = 0.1;
+        current_scene.camera.maxZ = 100.0;
+
+        // Create point light sources
+        let light1 = new PointLight('light1', new Vector3(1.0, 1.0, 5.0), scene);
+        light1.diffuse = new Color3(1.0, 1.0, 0.8);
+        light1.specular = new Color3(1.0, 1.0, 0.8);
+        current_scene.lights.push(light1);
+
+        // Create ground mesh
         let white_texture = RawTexture.CreateRGBTexture(new Uint8Array([255, 255, 255]), 1, 1, scene);
-
         let ground_heightmap = new Texture('/heightmaps/default.png', scene);
         ground_mesh.scaling = new Vector3(20.0, 1.0, 20.0);
-        ground_mesh.material = materials['ground_gauroud'];
-        materials['ground_gauroud'].setVector3('mat_color', new Vector3(0.10, 0.65, 0.15));
-        materials['ground_gauroud'].setVector3('mat_specular', new Vector3(0.0, 0.0, 0.0));
-        materials['ground_gauroud'].setFloat('mat_shininess', 1.0);
-        materials['ground_gauroud'].setFloat('height_scalar', 1.0); 
-        materials['ground_gauroud'].setTexture('heightmap', ground_heightmap);
-        materials['ground_gauroud'].setTexture('image', white_texture);
+        ground_mesh.metadata = {
+            mat_color: new Color3(0.10, 0.65, 0.15),
+            mat_texture: white_texture,
+            mat_specular: new Color3(0.0, 0.0, 0.0),
+            mat_shininess: 1,
+            height_scalar: 1.0,
+            heightmap: ground_heightmap
+        }
+        ground_mesh.material = materials['ground_gouraud'];
+
+        // Create other models
+        let sphere = CreateSphere('sphere', {segments: 32}, scene);
+        sphere.position = new Vector3(1.0, 0.5, 3.0);
+        sphere.metadata = {
+            mat_color: new Color3(0.10, 0.35, 0.88),
+            mat_texture: white_texture,
+            mat_specular: new Color3(0.8, 0.8, 0.8),
+            mat_shininess: 16,
+        }
+        sphere.material = materials['ground_gouraud'];
+
+
+        // Animation function - called before each frame gets rendered
+        scene.onBeforeRenderObservable.add(() => {
+            this.updateShaderUniforms(scene_idx, materials['illum_gouraud']);
+            this.updateShaderUniforms(scene_idx, materials['illum_phong']);
+            this.updateShaderUniforms(scene_idx, materials['ground_gouraud']);
+            this.updateShaderUniforms(scene_idx, materials['ground_phong']);
+        });
+    }
+
+    updateShaderUniforms(scene_idx, shader) {
+        let current_scene = this.scenes[scene_idx];
+        shader.setVector3('camera_position', current_scene.camera.position);
+        shader.setColor3('ambient', current_scene.scene.ambientColor);
+        shader.setInt('num_lights', current_scene.lights.length);
+        let light_positions = [];
+        let light_colors = [];
+        current_scene.lights.forEach((light) => {
+            light_positions.push(light.position.x, light.position.y, light.position.z);
+            light_colors.push(light.diffuse);
+        });
+        shader.setArray3('light_positions', light_positions);
+        shader.setColor3Array('light_colors', light_colors);
     }
 
     getActiveScene() {
